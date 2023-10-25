f7f7f0fdb2c6852c003322638d41eefa9ac7ad27
firstt <khi05040@naver.com>
2023-10-25 17:42:48 +0900

    10월 25일 복습

--- src/main/java/com/green/second/ch4/FlowEx1.java
+++ src/main/java/com/green/second/ch4/FlowEx1.java
@@ -7,17 +7,6 @@ public class FlowEx1 {
         Scanner scan = new Scanner(System.in);
         System.out.print("숫자를 하나 입력하세요 > ");
         String input = scan.nextLine();
-        //추가지령1 nextInt() 메소드 사용금지
-        //2 콘솔 입력 때 문자가 섞여 있으면 "잘못된 입력입니다." 출력
-        /*if () {
-            int intVal = Integer.parseInt(input);
-            if (intVal % 2 == 0 && intVal != 0) {
-                System.out.println("짝수입니다.");
-            } else if (intVal % 2 == 1) {
-                System.out.println("홀수입니다.");
-            } else if (input ==)
-                System.out.println("잘못된 입력입니다.");
-        }*/
 
         try {
             int intVal = Integer.parseInt(input);
